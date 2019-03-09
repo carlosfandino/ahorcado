4d2f4c90f37928c25ae9f5b64caf25069c474274
carlosfandino <48300255+carlosfandino@users.noreply.github.com>
2019-03-09 12:19:18 -0500

    Creacion del repositorio ahorcado
    Se crea la actividad ahorcado

---
+++ ahorcado_resources/js/ardoraAforCFG.js
@@ -0,0 +1,32 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=6; attempts=0; attemptsMax=1;
+var score=0; scoreMax=6; scoreInc=1; scoreDec=0
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=false;
+var textButtonTime="Comenzar";
+var tiSuccesses=true;
+var tiAttempts=false;
+var tiScore=true;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#0080FF"; colorText="#000000"; colorSele="#00FF00";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="Muy bien!!! :)"; messageTime="Se acabó el tiempo :("; messageError="Incorrecto :("; messageErrorG="Incorrecto :("; messageAttempts="Se acabaron los intentos :("; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="YWhvcmNhZG8"; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var words=["RkFUSEVS","TU9USEVS","R1JBTkRGQVRIRVI","R1JBTkRNT1RIRVI","QlJPVEhFUg","U0lTVEVS","","","",""];imaW=["0_father.jpg","0_mother.jpg","0_grandfather.jpg","0_abuela.jpg","0_brother.jpg","0_sister.jpg","","","",""];queW=["Pista: Padre","Pista: Madre","Pista: Abuelo","Pista: Abuela","Pista: Hermano","Pista: Hermana","","","",""];
+var auW=["","","","","","","","","",""];
+var fillLetter="ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚÜ "; wordsG=[]; indexG=0; actMaxWidth=550; indexWord=0; totalWord=0;
+var imageW=[];questionW=[];audioW=[];profG=0; dirMedia="ahorcado_resources/media/"; textBNext="Siguiente";
